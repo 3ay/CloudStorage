825c2a6c6536fb2569d429941f649e9f25b66e77
Alexey Zaitsev <39049885+3ay@users.noreply.github.com>
2023-12-05 21:27:07 +0300

    upd auth

--- src/main/java/ru/netology/cloudstorage/configuration/SecurityConfig.java
+++ src/main/java/ru/netology/cloudstorage/configuration/SecurityConfig.java
@@ -1,8 +1,6 @@
 package ru.netology.cloudstorage.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
--- src/main/java/ru/netology/cloudstorage/security/JwtTokenFilter.java
+++ src/main/java/ru/netology/cloudstorage/security/JwtTokenFilter.java
@@ -3,7 +3,6 @@ package ru.netology.cloudstorage.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
